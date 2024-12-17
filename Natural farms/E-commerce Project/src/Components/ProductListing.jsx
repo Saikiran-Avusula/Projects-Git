339dc72c05b566ea05d842c63b28de1a660123cd
Saikiran-Avusula <saikiranavusula89@gmail.com>
2024-12-17 13:16:55 +0530

    loader

--- Natural farms/E-commerce Project/src/Components/ProductListing.jsx
+++ Natural farms/E-commerce Project/src/Components/ProductListing.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Product from "./Product"
+import { ThreeCircles } from "react-loader-spinner"
 
 
 
@@ -20,21 +21,42 @@ const ProductListing = () => {
     return (
         <>
             {/* <h1 style={{ textAlign: 'center', }}>Product List Page</h1> */}
-            <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: 2 }}>
-                {products.map(item =>
-                    <Product
-                        key={item.id}
-                        id={item.id} //id also should pass as prop to cart.jsx
-                        title={item.title}
-                        price={item.price}
-                        category={item.category}
-                        description={item.description}
-                        image={item.image}
-                        rating={item.rating.rate}
-                        stock={item.rating.count}
-                    />)
+            <div>
+                {products.length > 0 ?
+                    <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: 2 }}>
+                        {products.map(item =>
+                            <Product
+                                key={item.id}
+                                id={item.id} //id also should pass as prop to cart.jsx
+                                title={item.title}
+                                price={item.price}
+                                category={item.category}
+                                description={item.description}
+                                image={item.image}
+                                rating={item.rating.rate}
+                                stock={item.rating.count}
+                            />)
+                        }
+                    </div>
+                    :
+                    <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: 2, padding: "20rem" }}>
+                        <ThreeCircles
+                            visible={true}
+                            height="100"
+                            width="100"
+                            color="#4fa94d"
+                            ariaLabel="three-circles-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                        />
+                    </div>
+
+
                 }
             </div>
+
+
+
 
         </>
     )
